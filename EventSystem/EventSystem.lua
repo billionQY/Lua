67ecd2cd828c508ba73e7b485661f2ca2155d363
billionQY <billionQY@gmail.com>
2021-01-12 18:01:51 +0800

    Create EventSystem.lua

---
+++ EventSystem/EventSystem.lua
@@ -0,0 +1,46 @@
+require("EventID")
+
+EventSystem = {}
+
+local map
+
+function EventSystem:new()
+    map = {}
+end
+
+function EventSystem:dispose()
+    map = nil
+end
+
+function EventSystem:publish(event_id, ...)
+    local lst = map[event_id]
+    if (lst ~= nil) then
+        for k, v in pairs(lst) do
+            v(...)
+        end
+    end
+end
+
+function EventSystem:subscribe(event_id, callback)
+    local lst = map[event_id]
+    if (lst == nil) then
+        lst = {}
+        map[event_id] = lst
+    end
+
+    table.insert(lst, callback)
+end
+
+function EventSystem:unsubscribe(event_id, callback)
+    local lst = map[event_id]
+    if (lst ~= nil) then
+        for k, v in pairs(lst) do
+            if (callback == v) then
+                table.remove(lst, k)
+                return
+            end
+        end
+    end
+end
+
+return EventSystem
