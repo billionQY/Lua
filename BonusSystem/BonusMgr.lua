45a3fe0a7c4338224ae5c33294bf934b5e2d3807
billionQY <billionQY@gmail.com>
2021-01-13 16:27:02 +0800

    Update BonusMgr.lua

--- BonusSystem/BonusMgr.lua
+++ BonusSystem/BonusMgr.lua
@@ -4,23 +4,23 @@ require("BonusSystem/BonusFuncs/BonusFunc_Gold")
 
 BonusMgr = {}
 
-local map;
+local map
 
 function BonusMgr:init()
-    map = {};
-    map[BonusFunc_Coin:bonus_type()] = BonusFunc_Coin;
-    map[BonusFunc_Gold:bonus_type()] = BonusFunc_Gold;
+    map = {}
+    map[BonusFunc_Coin:bonus_type()] = BonusFunc_Coin
+    map[BonusFunc_Gold:bonus_type()] = BonusFunc_Gold
 end
 
 function BonusMgr:dispose()
-    map = nil;
+    map = nil
 end
 
 function BonusMgr:do_bonus(bonus_type, ...)
     local x = map[bonus_type];
     if (x ~= nil) then
-        x:do_bonus(...);
+        x:do_bonus(...)
     end
 end
 
-return BonusMgr;
+return BonusMgr
