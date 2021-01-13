23099c964db356e15c055c76f86ac43b1a2ef77a
billionQY <billionQY@gmail.com>
2021-01-13 16:23:20 +0800

    Create BonusMgr.lua

---
+++ BonusSystem/BonusMgr.lua
@@ -0,0 +1,26 @@
+require("BonusSystem/BonusType")
+require("BonusSystem/BonusFuncs/BonusFunc_Coin")
+require("BonusSystem/BonusFuncs/BonusFunc_Gold")
+
+BonusMgr = {}
+
+local map;
+
+function BonusMgr:init()
+    map = {};
+    map[BonusFunc_Coin:bonus_type()] = BonusFunc_Coin;
+    map[BonusFunc_Gold:bonus_type()] = BonusFunc_Gold;
+end
+
+function BonusMgr:dispose()
+    map = nil;
+end
+
+function BonusMgr:do_bonus(bonus_type, ...)
+    local x = map[bonus_type];
+    if (x ~= nil) then
+        x:do_bonus(...);
+    end
+end
+
+return BonusMgr;
